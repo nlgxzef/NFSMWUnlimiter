cd919453297ffe33e50ba1fd59d057e42771359f
Anıl Gezergen <nlgzrgn@outlook.com>
2018-09-23 13:30:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NFSMW Unlimiter
+NFSMW Unlimiter is a script mod which fixes some issues for added cars:
++ Added cars cannot be seen in game. (Increased CarTypeID limit and block size variables to solve the problem.)
++ Selecting added cars make the game crash. (Increased CarTypeID limit for RenderConn functions.)
+- Cars over the limit, or models don't exist in CARS folder will get replaced with Porsche Carrera GT.
+
+# Download
+You can [download Unlimiter](https://github.com/ExOptsTeam/NFSMWExOpts/releases) from Releases page, or from [NFSMods.xyz](https://www.nfsmods.xyz).
+If you want to compile it yourself, you can download the source code from the green Clone or Download button up there.
---
+++ dllmain.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include "stdio.h"
+#include <windows.h>
+#include "..\includes\injector\injector.hpp"
+#include "..\includes\IniReader.h"
+
+#define CarCount 127 // MAX = 127
+#define ReplacecmentCar 1 // CARRERAGT
+
+int Init()
+{
+	injector::WriteMemory<BYTE>(0x6690BD, CarCount, true); // PVehicle::Resource::Resource
+	injector::WriteMemory<BYTE>(0x7398A8, CarCount, true);
+	injector::WriteMemory<int>(0x739900, CarCount * 0xD0, true); // CarPartDatabase::GetCarType
+	injector::WriteMemory<int>(0x739909, ReplacecmentCar, true); // Replacement model if model not found
+	injector::WriteMemory<BYTE>(0x73995D, CarCount, true); // GetCarTypeInfoFromHash
+	injector::WriteMemory<BYTE>(0x7515FD, CarCount, true);
+	injector::WriteMemory<BYTE>(0x759915, CarCount, true); // RideInfo::FillWithPreset
+	injector::WriteMemory<unsigned char>(0x75E6F6, CarCount, true); // CarRenderConn::Construct
+	injector::WriteMemory<unsigned char>(0x75E7B6, CarCount, true); // HeliRenderConn::Construct
+	injector::WriteMemory<int>(0x7B3879, CarCount * 0xD0, true); // DebugCarCustomizeScreen::BuildOptionsLists
+	return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		Init();
+	}
+	return TRUE;
+}
+
